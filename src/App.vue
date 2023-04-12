9a9b224c6464695cdf9286866db8164b2440ae8b
Daniel Wang <h3786010@yahoo.com.tw>
2023-04-12 11:26:49 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { onMounted } from 'vue'
+import { CapacitorHttp } from '@capacitor/core'
+
+onMounted(async () => {
+
+  await CapacitorHttp.post({
+    url: 'https:/mydomain/api/authentication/login',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    webFetchExtra: {
+      credentials: "include",
+    },
+  }).then((response) => {
+    alert(JSON.stringify(response.data));
+  }).catch((error) => {
+    alert(error.message);
+  })
+
+})
+</script>
+
+<template>
+	hello world!
+</template>
+
